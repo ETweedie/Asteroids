94c20123f75dcdcbe7c8b8d5cc180ae13a887dc9
ETweedie <ericttweedie131@gmail.com>
2025-10-05 10:35:12 -0400

    Update to main with groups

--- main.py
+++ main.py
@@ -4,25 +4,34 @@ from player import Player
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, color="black")
-        player.draw(screen)
-        pygame.display.flip()
-        dt = clock.tick(60)/1000.0
-        player.update(dt)
-        
-        
-        
+            
+        updateable.update(dt)
 
+        screen.fill("black")
+        
+        for item in drawable:
+            item.draw(screen)
+        
+        pygame.display.flip()
+
+        dt = clock.tick(60)/1000.0
+        
+        
+        
 if __name__ == "__main__":
     main()
